6f79d4ddf58d8e57d265548bda9078117233d981
AbdullahUmer575 <abdullahumer575@gmail.com>
2023-05-04 01:44:27 +0500

    back on track, home screen complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,55 @@
 import {Routes, Route, Link, Outlet} from 'react-router-dom';
 import './App.css'
+import pic from './assets/profile.png'
 
 export default function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Basic Example</h1>
-
-      <p>
-        This example demonstrates some of the core features of React Router
-        including nested <code>&lt;Route&gt;</code>s,{" "}
-        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
-        "*" route (aka "splat route") to render a "not found" page when someone
-        visits an unrecognized URL.
-      </p>
-
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} />
-
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
-          <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </div>
   )
 }
 
 function Layout() {
   return (
-    <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
+    <div className="layout">
+      <div className="navbar">
+        <div className="logo">
+          Document Dispatch Register
+        </div>
+        <img className="profilePic" src={pic}></img>
+        <div className="welcome">
+          Welcome Ali!
+        </div>
+      </div>
       <Outlet />
+      <div className="footer">
+      </div>
     </div>
   );
 }
 
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="home">
+      <button className = "homeButtons">
+      <Link to="/">Pending Applications</Link>
+      </button>
+      <button className = "homeButtons">
+      <Link to="/">Submit Application</Link>
+      </button>
     </div>
   );
 }
 
 function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
-
-function NoMatch() {
   return (
     <div>
       <h2>Nothing to see here!</h2>
