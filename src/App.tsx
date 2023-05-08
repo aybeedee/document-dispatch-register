f246b5ec72b29bd2315ff3f7bdd31b3ca50736ae
AbdullahUmer575 <abdullahumer575@gmail.com>
2023-05-09 00:45:17 +0500

    added search functionalities for applications

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {Routes, Route, Link, Outlet, useNavigate} from 'react-router-dom';
-import {useState} from 'react';
+import {useState, useMemo} from 'react';
 import './App.css';
 import pic from './assets/profile.png';
 import logo from './assets/logo.png';
@@ -25,8 +25,95 @@ const applications: any = [
     department: "Computing",
     applicationDate: "2023-03-17",
     description: "Submitted fine amount with requied documents."
+  },
+
+  {
+    docID: nanoid(),
+    docType: "Grade Change",
+    applicantName: "Asif Khan",
+    applicantID: "20I-4534",
+    department: "FSM",
+    applicationDate: "2023-05-01",
+    description: "Application for change of grade."
+  },
+
+  {
+    docID: nanoid(),
+    docType: "Teacher Leave",
+    applicantName: "Rahmanullah Gurbaz",
+    applicantID: "E24352",
+    department: "Computing",
+    applicationDate: "2023-01-04",
+    description: "Important Training Summit."
+  },
+
+  {
+    docID: nanoid(),
+    docType: "Course Withdrawal",
+    applicantName: "Rafay Sheikh",
+    applicantID: "22I-2311",
+    department: "FSM",
+    applicationDate: "2023-04-17",
+    description: "Urgent."
+  },
+
+  {
+    docID: nanoid(),
+    docType: "Disciplinary Fine",
+    applicantName: "Talha Tahir",
+    applicantID: "18I-0532",
+    department: "Computing",
+    applicationDate: "2023-05-09",
+    description: "Submitted fine amount."
   }
 ];
+
+const tracks: any = [
+
+  {
+    title: "Course Withdrawal",
+    authorities: [
+      "Officer Saif Sheikh",
+      "Coordinator Zubair Rahim",
+      "HOD Rahman Zahid"
+    ]
+  },
+
+  {
+    title: "Absence Justification",
+    authorities: [
+      "Officer Saif Sheikh",
+      "HOD Tanveer Rahim"
+    ]
+  },
+
+  {
+    title: "Teacher Leave",
+    authorities: [
+      "Officer Saif Sheikh",
+      "HOD Zubair Rahim",
+      "HOS Talha Fazal"
+    ]
+  },
+
+  {
+    title: "Grade Change",
+    authorities: [
+      "Officer Saif Sheikh",
+      "Coordinator Rehman Ullah",
+      "HOD Tanveer Rahim"
+    ]
+  },
+
+  {
+    title: "Disciplinary Fine",
+    authorities: [
+      "Officer Saif Sheikh",
+      "DC-Incharge Yasir Ali",
+      "HOS Talha Fazal"
+    ]
+  }
+]
 
 export default function App() {
 
@@ -149,11 +236,138 @@ function SubmitApplication() {
 }
 
 function PendingApplications() {
+
+  const [searchTerms, setSearchTerms] = useState({
+
+    applicantName: "",
+    applicantID: "",
+    startDate: "",
+    endDate: "",
+    department: "All"
+  })
+
+  const filteredApplications = applications.filter(
+    (application : any) => {
+
+      if (searchTerms.startDate && searchTerms.endDate) {
+
+        if (searchTerms.department === "All") {
+
+          return (
+            application.applicantName.toLowerCase().includes(searchTerms.applicantName.toLowerCase()) &&
+            application.applicantID.toLowerCase().includes(searchTerms.applicantID.toLowerCase()) &&
+            ((application.applicationDate >= searchTerms.startDate) && (application.applicationDate <= searchTerms.endDate))
+          );
+        }
+
+        else {
+
+          return (
+            application.applicantName.toLowerCase().includes(searchTerms.applicantName.toLowerCase()) &&
+            application.applicantID.toLowerCase().includes(searchTerms.applicantID.toLowerCase()) &&
+            (application.department === searchTerms.department) &&
+            ((application.applicationDate >= searchTerms.startDate) && (application.applicationDate <= searchTerms.endDate))
+          );
+        }
+      } 
+      
+      else if (searchTerms.startDate) {
+
+        if (searchTerms.department === "All") {
+
+          return (
+            application.applicantName.toLowerCase().includes(searchTerms.applicantName.toLowerCase()) &&
+            application.applicantID.toLowerCase().includes(searchTerms.applicantID.toLowerCase()) &&
+            (application.applicationDate >= searchTerms.startDate)
+          );
+        }
+
+        else {
+
+          return (
+            application.applicantName.toLowerCase().includes(searchTerms.applicantName.toLowerCase()) &&
+            application.applicantID.toLowerCase().includes(searchTerms.applicantID.toLowerCase()) &&
+            (application.department === searchTerms.department) &&
+            (application.applicationDate >= searchTerms.startDate)
+          );
+        }
+      } 
+      
+      else if (searchTerms.endDate) {
+
+        if (searchTerms.department === "All") {
+
+          return (
+            application.applicantName.toLowerCase().includes(searchTerms.applicantName.toLowerCase()) &&
+            application.applicantID.toLowerCase().includes(searchTerms.applicantID.toLowerCase()) &&
+            (application.applicationDate <= searchTerms.endDate)
+          );
+        }
+
+        else {
+
+          return (
+            application.applicantName.toLowerCase().includes(searchTerms.applicantName.toLowerCase()) &&
+            application.applicantID.toLowerCase().includes(searchTerms.applicantID.toLowerCase()) &&
+            (application.department === searchTerms.department) &&
+            (application.applicationDate <= searchTerms.endDate)
+          ); 
+        }
+      } 
+      
+      else {
+
+        if (searchTerms.department === "All") {
+
+          return (
+            application.applicantName.toLowerCase().includes(searchTerms.applicantName.toLowerCase()) &&
+            application.applicantID.toLowerCase().includes(searchTerms.applicantID.toLowerCase())
+          );
+        }
+
+        else {
+
+          return (
+            application.applicantName.toLowerCase().includes(searchTerms.applicantName.toLowerCase()) &&
+            application.applicantID.toLowerCase().includes(searchTerms.applicantID.toLowerCase()) &&
+            (application.department === searchTerms.department)
+          );
+        }
+      }
+    }
+  );
+
+  const handleChange = (e: any) => {
+
+    setSearchTerms(prev => ({...prev, [e.target.name]: e.target.value}));
+  };
+
   return (
     <div>
       <div className="content">
           <div className="container">
               <h2>Applications</h2>
+              <div className = "searchParams">
+                <input type = "text" placeholder = "Applicant Name" onChange = {handleChange} name = "applicantName"/>
+                <input type = "text" placeholder = "Applicant ID" onChange = {handleChange} name = "applicantID"/>
+                <div>
+                  <label>Start Date:  </label>
+                  <input id = "startDate" type = "date" onChange = {handleChange} name = "startDate"/>
+                </div>
+                <div>
+                  <label>End Date:  </label>
+                  <input id = "endDate" type = "date" onChange = {handleChange} name = "endDate"/>
+                </div>
+                <select onChange = {handleChange} name = "department">
+                  <option selected value = "All"> -- All Departments -- </option>
+                  <option value = "Accounts">Accounts</option>
+                  <option value = "Admin">Admin</option>
+                  <option value = "HR">HR</option>
+                  <option value = "Computing">Computing</option>
+                  <option value = "EE">EE</option>
+                  <option value = "FSM">FSM</option>
+                </select>
+              </div>
               <div>
                   <table className="table custom-table">
                   <thead>
@@ -168,7 +382,7 @@ function PendingApplications() {
                   </thead>
                   <tbody>
                       {
-                          applications.map((application: any) => (
+                          filteredApplications.map((application: any) => (
                               <tr key={application.docID}>
                                   <td>{application.docType}</td>
                                   <td>{application.department}</td>
